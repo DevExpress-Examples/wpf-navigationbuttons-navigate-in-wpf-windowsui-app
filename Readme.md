133e74fd34d24cd96f10a52b617018c95758b1f3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How To: Use NavigationButtons to Navigate in WPF WindowsUI Applications
+
+
+<p>This example demonstrates how to use NavigationButtons to navigate through views in WPF WindowsUI applications.</p>
+
+<br/>
+
+
---
+++ VB/NavigationButtons/App.xaml.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Threading.Tasks
+Imports System.Windows
+
+Namespace NavigationButtons
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Public Partial Class App
+		Inherits Application
+	End Class
+End Namespace
